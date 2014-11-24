f9ce918e35d12286c35bd97ebe36bf0819096a21
Felipe Besson <flpbesson@gmail.com>
2014-11-24 21:17:44 -0200

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122232455) do
+ActiveRecord::Schema.define(version: 20141124230823) do
 
   create_table "bubbles", force: true do |t|
-    t.string "name"
-    t.float  "value"
+    t.string  "name"
+    t.float   "value"
+    t.integer "metric_id"
   end
 
   create_table "categories", force: true do |t|
