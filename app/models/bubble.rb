2417e822f03d07c3c48b22f2c1dff305d4f94d61
Felipe Besson <flpbesson@gmail.com>
2014-11-30 15:59:01 -0200

    add location model

--- app/models/bubble.rb
+++ app/models/bubble.rb
@@ -1,5 +1,10 @@
 class Bubble < ActiveRecord::Base
   belongs_to :metric
+  belongs_to :location
   has_many :categorizations
   has_many :categories, through: :categorizations
+
+  def to_map
+    self
+  end
 end
