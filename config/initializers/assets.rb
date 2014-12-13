b0e27df60775d7f970cf701067f6ee5f234ec0f5
Felipe Besson <flpbesson@gmail.com>
2014-12-13 19:16:08 -0200

    start jobblee

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,10 @@ Rails.application.config.assets.precompile += %w( datamaps.all.min.js )
 Rails.application.config.assets.precompile += %w( map.js )
 Rails.application.config.assets.precompile += %w( chart.js )
 
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( agency.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
